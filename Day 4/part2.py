a27c40092f6263a98a100281ca79717c105f4b74
Rayaan Afzal <rafzal2014@gmail.com>
2024-12-05 12:15:12 -0500

    Solved Day 4

--- Day 4/part2.py
+++ Day 4/part2.py
@@ -3,46 +3,74 @@ word = "MAS"
 M   S
   A
 M   S
+
+"""
+"""
+S   M
+  A 
+S   M
 """
 
-grid =[]
-marked_a = []
+"""
+M   M
+  A
+S   S
+"""
+"""
+S   S
+  A 
+M   M
+"""
+
+grid = []
 with open("input.txt", 'r') as file:
     for line in file:
         grid.append(list(line.strip()))
 
+
 def search_xmas(grd):
     count = 0
     rows, cols = len(grd), len(grd[0])
-    directions = [(1,1),(1,-1)]
 
-    def is_valid(x,y):
+    def is_valid(x, y):
         return 0 <= x < rows and 0 <= y < cols
 
-    def is_valid_x(x,y,dx,dy):
-            nx = x
-            ny = y
-            for i in range(len(word)):
-                if not is_valid(nx,ny) or grd[nx][ny] != word[i]:
-                    break
-                if word[i] == 'A':
-                    marked_a.append(i)
+    def is_valid_x(x, y):
+        if not is_valid(x, y) or grd[x][y] != 'A':
+            return False
 
-                nx += dx
-                ny += dy
-            else:
+        if is_valid(x, y) and grd[x][y] == 'A':
+            # Check for case with M's on left side
+            if ((is_valid(x - 1, y - 1) and grd[x - 1][y - 1] == 'M' and is_valid(x + 1, y + 1) and grd[x + 1][
+                y + 1] == 'S') and
+                    (is_valid(x + 1, y - 1) and grd[x + 1][y - 1] == 'M' and is_valid(x - 1, y + 1) and grd[x - 1][
+                        y + 1] == 'S')):
+                return True
+            # Check for case with M's on right side
+            elif ((is_valid(x - 1, y + 1) and grd[x - 1][y + 1] == 'M' and is_valid(x + 1, y - 1) and grd[x + 1][
+                y - 1] == 'S') and
+                  (is_valid(x + 1, y + 1) and grd[x + 1][y + 1] == 'M' and is_valid(x - 1, y - 1) and grd[x - 1][
+                      y - 1] == 'S')):
+                return True
+            # Check for case with M's on the top
+            elif ((is_valid(x - 1, y - 1) and grd[x - 1][y - 1] == 'M' and is_valid(x + 1, y + 1) and grd[x + 1][
+                y + 1] == 'S') and
+                  (is_valid(x - 1, y + 1) and grd[x - 1][y + 1] == 'M' and is_valid(x + 1, y - 1) and grd[x + 1][
+                      y - 1] == 'S')):
+                return True
+            # Check for case with M's on the bottom
+            elif ((is_valid(x + 1, y - 1) and grd[x + 1][y - 1] == 'M' and is_valid(x - 1, y + 1) and grd[x - 1][
+                y + 1] == 'S') and
+                  (is_valid(x + 1, y + 1) and grd[x + 1][y + 1] == 'M' and is_valid(x - 1, y - 1) and grd[x - 1][
+                      y - 1] == 'S')):
                 return True
 
     for i in range(rows):
         for j in range(cols):
-            if grd[i][j] == word[0]:
-                if 0 < j + 2 < cols:
-                    if is_valid_x(i, j, 1, 1) and is_valid_x(i, j+2, 1, -1):
-                        count += 1
+            if is_valid_x(i, j):
+                count += 1
 
     return count
 
 
 print(search_xmas(grid))
-
-
